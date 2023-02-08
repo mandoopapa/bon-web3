543d39d238a96353bf4476e0e3628519b454723a
mandoopapa <remonytea@gmail.com>
2023-02-08 21:25:35 +0900

    기초 layout 작업

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './header.css';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div>
+                <h2>Header</h2>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import './layout.css'
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <div className="layout">
+            </div>
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
